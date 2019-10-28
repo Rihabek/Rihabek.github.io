ba4c99789233e0f76f7328802ec0194456563a61
rihab <rihabelkhadiri@hotmail.com>
2019-10-28 16:43:09 +0100

    et voila

---
+++ js/app.js
@@ -0,0 +1,13 @@
+// A $( document ).ready() block.
+$( document ).ready(function() {
+  $("#cv").click(function(){
+    $(this).css('color','#961108');
+    return false;
+  });
+});
+
+
+  $( function() {
+    $( "#menu" ).menu();
+  } );
+  
