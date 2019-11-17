f8e024c2c38aeac1bd422d327e8ee7a7fb67448c
rihab <rihabek>
2019-11-17 21:07:42 +0100

    ..

--- js/app.js
+++ js/app.js
@@ -19,9 +19,9 @@ $('.navbar-nav').click(function() {
   $('.navbar-collapse').collapse('hide');
 })
 
-$(function() {
-  $(".typed").typed({
-    strings: ["Typed.js is a jQuery plugin that types.", "Enter in any string,", "and watch it type at the speed you've set,", "backspace what it's typed,", "and begin a new sentence for however many strings you've set."],
-    typeSpeed: 0
-  });
-});
+// $(function() {
+//   $(".typed").typed({
+//     strings: ["Typed.js is a jQuery plugin that types.", "Enter in any string,", "and watch it type at the speed you've set,", "backspace what it's typed,", "and begin a new sentence for however many strings you've set."],
+//     typeSpeed: 0
+//   });
+// });
