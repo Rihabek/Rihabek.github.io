c20ae15f38e9ffa3987aff9579dcbf697597eb4c
rihab <rihabek>
2019-11-18 21:58:26 +0100

    ..

--- js/app.js
+++ js/app.js
@@ -14,10 +14,3 @@ $(this).closest(".dropdown").find(".dropdown-toggle").dropdown("toggle");
 $('.navbar-nav').click(function() {
   $('.navbar-collapse').collapse('hide');
 })
-
-// $(function() {
-//   $(".typed").typed({
-//     strings: ["Typed.js is a jQuery plugin that types.", "Enter in any string,", "and watch it type at the speed you've set,", "backspace what it's typed,", "and begin a new sentence for however many strings you've set."],
-//     typeSpeed: 0
-//   });
-// });
